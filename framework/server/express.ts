b859e7e761db4295d00dd1c3db5a5312763b0e40
i894 <i894@naver.com>
2022-09-29 18:12:00 +0900

    Add:View Engiene(ejs)

--- framework/server/express.ts
+++ framework/server/express.ts
@@ -17,8 +17,8 @@ class ExpressApp{
         app.use(express.json());
         app.use(express.urlencoded({extended:false}));
         app.use(cookieParser());
+        app.set('view engine','ejs');
         app.use(express.static(path.join(__dirname,'../../src/public')));
-        
         return app;
     }
 }
