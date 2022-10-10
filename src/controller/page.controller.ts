5c8e30c5e672a4f3a36e34209440787d2e0ad245
BangDori <qudwns8865@naver.com>
2022-10-10 15:15:07 +0900

    Add: user 로그인 후 시작하기 버튼 임시 활성화

--- src/controller/page.controller.ts
+++ src/controller/page.controller.ts
@@ -28,13 +28,12 @@ class PageController implements ControllerDefaultClass {
 
     private index(api:RouterApiSpec){
         return async (req:express.Request,res:express.Response)=>{
-            res.render('indexPage', {title: 'Express'});
+            res.render('indexPage', {id: null});
         }
     }
 
     private userIndex(api:RouterApiSpec) {
         return async (req:express.Request, res:express.Response) => {
-
             res.render('userPage');
         }
     }
