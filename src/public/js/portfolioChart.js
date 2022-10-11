3617043dd4682129e981b2d1bf6cd892d2869a4e
BangDori <qudwns8865@naver.com>
2022-10-11 12:10:59 +0900

    Add: 포트폴리오 비교&랭킹 창

--- src/public/js/portfolioChart.js
+++ src/public/js/portfolioChart.js
@@ -1,6 +1,6 @@
 anychart.onDocumentReady(function () {
     // create pie chart with passed data
-    var chart = anychart.pie([
+    const chart1 = anychart.pie([
       ['a', 1],
       ['b', 1],
       ['c', 1],
@@ -14,7 +14,7 @@ anychart.onDocumentReady(function () {
     ]);
 
     // set chart title text settings
-    chart
+    chart1
       .background({
         corners: 30,
         fill: 'transparent',
@@ -36,10 +36,64 @@ anychart.onDocumentReady(function () {
       // create empty area in pie chart
       .innerRadius('40%');
 
-    // set container id for the chart
-    chart.container('my_portfolio_graph');
-    // initiate chart drawing
-    chart.draw();
+    const chart2 = chart1;
+    chart2.title({
+      fontSize: '24px',
+    })
 
-    document.querySelector(".anychart-credits").innerHTML = "";
+    // set container id for the chart
+    chart1.container('my_portfolio_graph');
+    chart2.container('my_portfolio_graph_for_comparison')
+    // initiate chart drawing
+    chart1.draw();
+    chart2.draw();
+});
+
+anychart.onDocumentReady(function () {
+  // create pie chart with passed data
+  var chart = anychart.pie([
+    ['a', 5],
+    ['b', 2],
+    ['c', 3],
+    ['d', 1],
+    ['others', 1],
+  ]);
+
+  // set chart title text settings
+  chart
+    .background({
+      corners: 30,
+      fill: 'transparent',
+    })
+    .legend({
+      position: 'right',
+      itemsLayout: 'vertical-expandable',
+      height: '160px',
+      align: 'middle',
+   })
+    .title({
+      text: '유저 포트폴리오',
+      align: 'left',
+      fontColor: 'white',
+      fontSize: '24px',
+    })
+    // set chart radius
+    .radius('45%')
+    // create empty area in pie chart
+    .innerRadius('40%');
+
+  // set container id for the chart
+  chart.container('user_portfolio_graph');
+  // initiate chart drawing
+  chart.draw();
+
+  const removeAD = () => {
+    credits = document.querySelectorAll(".anychart-credits");
+  
+    console.log(credits);
+    credits.forEach(credit => {
+      credit.innerHTML = "";
+    })
+  }
+  removeAD();
 });
